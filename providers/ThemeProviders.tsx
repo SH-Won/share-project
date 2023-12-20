21cbac100d865cbc05c700d1610b71683d816fd8
SeongHo-Won <1317good@naver.com>
2023-12-20 15:12:48 +0900

    #42 re deploy

--- providers/ThemeProviders.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import { theme } from '@/styles/theme'
-import { ThemeProvider } from 'styled-components'
-
-const ThemeProviders = ({ children }: { children: React.ReactNode }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>
-}
-export { ThemeProviders }
